d7fbd1c448c84a752697acb5f5318cee27b9ab8c
Elli Balcomb <elli.balcomb@dabohealth.com>
2014-06-13 15:47:05 -0700

    added static pages

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,7 +12,7 @@
 
 def secure_token
   token_file = Rails.root.join('.secret')
-  if File.exist(token_file)
+  if File.exist?(token_file)
     File.read(token_file).chomp
   else
     token = SecureRandom.hex(64)
